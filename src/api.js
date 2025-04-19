500b2f4550affa781bd02da95bccb5ecd611286a
Orlando Pernia <69866332+njrzr@users.noreply.github.com>
2025-04-18 20:41:24 -0400

    Added achievement combination for xbox 360 titles.

--- src/api.js
+++ src/api.js
@@ -17,6 +17,7 @@ router.all("/xbl", cors({ origin: process.env.ORIGIN }), async (req, res) => {
   };
 
   let data;
+  let second;
 
   await needle(req.method, req.header('Target-URL'), {
     headers: {
@@ -24,8 +25,28 @@ router.all("/xbl", cors({ origin: process.env.ORIGIN }), async (req, res) => {
     }
   }
   )
-    .then(res => data = data = res.body)
+    .then(res => data = res.body)
     .catch(err => res.json(err))
+
+  if (req.header('Target-URL').match('x360') != null) {
+    await needle(req.method, req.header('Target-URL').replace('x360', 'player'), {
+      headers: {
+        "X-Authorization": req.header('X-Authorization'),
+      }
+    }
+    )
+      .then(res => second = res.body)
+      .catch(err => res.json(err))
+
+    const map = new Map();
+
+    second.achievements.concat(data.achievements).forEach(item => {
+      map.set(item.id, item);
+    });
+
+    const mergedArray = Array.from(map.values());
+    data["achievements"] = mergedArray;
+  }
 
   res.json(data);
 });
